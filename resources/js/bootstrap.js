76704938c7a6f1f4c47cea6c8ba2de5b8b812c6b
PEtcheberry <pablo.etcheberry@usm.cl>
2025-02-03 18:22:57 -0300

    Fixed soketi connection

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,25 +9,16 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // Configurar Pusher
 window.Pusher = Pusher;
 
-// Obtener variables de entorno desde Vite
-const PUSHER_APP_KEY = import.meta.env.VITE_PUSHER_APP_KEY || 'app-key';
-const PUSHER_HOST = import.meta.env.VITE_PUSHER_HOST || '127.0.0.1';
-const PUSHER_PORT = import.meta.env.VITE_PUSHER_PORT || 6001;
-const PUSHER_SCHEME = import.meta.env.VITE_PUSHER_SCHEME || 'http';
-const PUSHER_APP_CLUSTER = import.meta.env.VITE_PUSHER_APP_CLUSTER || 'mt1';
-
 // Configurar Laravel Echo con Soketi
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.PUSHER_APP_KEY, // Clave de Pusher
-    wsHost: PUSHER_HOST, // Host de WebSocket
-    wsPort: PUSHER_PORT, // Puerto de WebSocket
-    wssPort: PUSHER_PORT, // Puerto seguro de WebSocket
-    forceTLS: PUSHER_SCHEME === 'https', // Forzar TLS si el esquema es HTTPS
-    disableStats: true, // Deshabilitar estadísticas
-    cluster: PUSHER_APP_CLUSTER, // Cluster de Pusher (para compatibilidad)
-    encrypted: false,
-    enabledTransports: ['ws', 'wss'], // Habilitar WebSocket y WebSocket seguro
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+    wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
 });
 
 // Escuchar errores de conexión
@@ -37,9 +28,12 @@ window.Echo.connector.pusher.connection.bind('error', (err) => {
 
 // Suscribirse al canal 'ventas'
 window.Echo.channel('ventas')
-    .subscribed(() => console.log("✅ Suscrito al canal Ventas"))
-    .listen('.VentasChannelSubscribed', (data) => {
-        console.log('📩 Evento recibido:', data);
-    });
+    .listen('.MessageSent', (data) => {
+        console.log('Mensaje recibido:', data);
+        const messagesList = document.getElementById('messagesList');
+        const li = document.createElement('li');
+        li.textContent = `${data.user.name}: ${data.message}`;
+        messagesList.appendChild(li);
+});
 
 console.log("🔄 Intentando conectar al WebSocket...");
